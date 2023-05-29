5acb82fafe6f3f11d9e6568ece4143d81498ef48
inwookjung <inwook.jung@kr.doubledown.com>
2023-05-29 20:46:01 +0900

    수정1

--- cicd/serverBatchDeployScript.groovy
+++ cicd/serverBatchDeployScript.groovy
@@ -90,24 +90,24 @@ pipeline {
             steps {
                 def script = """
                         #!/bin/bash
-                            ORIGIN_JAR_PATH='/var/jenkins_home/batch/deploy/*.jar'
-                            ORIGIN_JAR_NAME=\$(basename ${ORIGIN_JAR_PATH})
-                            TARGET_PATH='/home/jenkins_home/batch/application.jar'
-                            JAR_BOX_PATH='/home/jenkins_home/batch/jar/'
-                            
-                            echo "  > 배포 JAR: "${ORIGIN_JAR_NAME}
-                            
-                            echo "  > chmod 770 ${ORIGIN_JAR_PATH}"
-                            sudo chmod 770 ${ORIGIN_JAR_PATH}
-                            
-                            echo "  > cp ${ORIGIN_JAR_PATH} ${JAR_BOX_PATH}"
-                            sudo cp ${ORIGIN_JAR_PATH} ${JAR_BOX_PATH}
-                            
-                            echo "  > chown -h jenkins:jenkins ${JAR_BOX_PATH}${ORIGIN_JAR_NAME}"
-                            sudo chown -h jenkins:jenkins ${JAR_BOX_PATH}${ORIGIN_JAR_NAME}
-                            
-                            echo "  > sudo ln -s -f ${JAR_BOX_PATH}${ORIGIN_JAR_NAME} ${TARGET_PATH}"
-                            sudo ln -s -f ${JAR_BOX_PATH}${ORIGIN_JAR_NAME} ${TARGET_PATH}
+                        ORIGIN_JAR_PATH='/var/jenkins_home/batch/deploy/*.jar'
+                        ORIGIN_JAR_NAME=\$(basename ${ORIGIN_JAR_PATH})
+                        TARGET_PATH='/home/jenkins_home/batch/application.jar'
+                        JAR_BOX_PATH='/home/jenkins_home/batch/jar/'
+                        
+                        echo "  > 배포 JAR: "${ORIGIN_JAR_NAME}
+                        
+                        echo "  > chmod 770 ${ORIGIN_JAR_PATH}"
+                        sudo chmod 770 ${ORIGIN_JAR_PATH}
+                        
+                        echo "  > cp ${ORIGIN_JAR_PATH} ${JAR_BOX_PATH}"
+                        sudo cp ${ORIGIN_JAR_PATH} ${JAR_BOX_PATH}
+                        
+                        echo "  > chown -h jenkins:jenkins ${JAR_BOX_PATH}${ORIGIN_JAR_NAME}"
+                        sudo chown -h jenkins:jenkins ${JAR_BOX_PATH}${ORIGIN_JAR_NAME}
+                        
+                        echo "  > sudo ln -s -f ${JAR_BOX_PATH}${ORIGIN_JAR_NAME} ${TARGET_PATH}"
+                        sudo ln -s -f ${JAR_BOX_PATH}${ORIGIN_JAR_NAME} ${TARGET_PATH}
                         """.stripIndent()
                 writeFile(file: 'deploy/deploy.sh', text: script)
 
